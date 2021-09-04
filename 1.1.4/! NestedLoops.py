e047c350883b24830ee642a9464559c190372c5d
Andrew Guernsey <andrewgguernsey@gmail.com>
2021-09-04 18:17:00 -0500

    Nested and flipped

--- 1.1.4/! NestedLoops.py
+++ 1.1.4/! NestedLoops.py
@@ -6,13 +6,18 @@ painter.shape("circle")
 painter.hideturtle()
 painter.penup()
 
-x = -200
-while (x < 200): 
-  x = x + 50
-  y = 200
-  painter.goto(x,y)
+x = 200
+while (x > -200): 
+  x = x - 50
+  y = -200
+  painter.goto(y,x)
   painter.color("red")
   painter.stamp()
+  while (y < 0):
+    y = y + 50
+    painter.goto(y,x)
+    painter.color("blue")
+    painter.stamp()
 
 
 wn = trtl.Screen()
