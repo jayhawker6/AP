3a8080e20b0eda71b6b26b193459c8b38a53677b
Andrew Guernsey <andrewgguernsey@gmail.com>
2021-09-04 14:21:50 -0500

    Cut down on bulk

--- Tower.py
+++ Tower.py
@@ -2,34 +2,11 @@
 import turtle
 ttl = turtle.Turtle()
 wn = turtle.Screen()
-
-ttl.right(90)
-def down(x):
-    ttl.forward(x)
-def up(x):
-    ttl.backward(x)
-def right(x):
-    ttl.left(90)
-    down(x)
-    ttl.right(90)
-def left(x):
-    ttl.right(90)
-    down(x)
-    ttl.left(90)
 def speed(x):
     ttl.speed(x)
 def size(x):
     ttl.pensize(x)
 # End of EZ turtle commands by Andrew G #
-ttl.color('red', 'yellow')
-ttl.begin_fill()
-speed(0)
-while True:
-    ttl.forward(200)
-    ttl.left(170)
-    if abs(ttl.pos()) < 1:
-        break
-ttl.end_fill()
-ttl.shape("classic")
+ttl.left(90)
 # END OF PROGRAM #
 wn.exitonclick()
