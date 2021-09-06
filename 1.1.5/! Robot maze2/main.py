a22bfc4105c90caa245aa4e53f164f9be76cc6f1
Andrew Guernsey <75534911+jayhawker6@users.noreply.github.com>
2021-09-06 15:20:55 -0500

    Add files via upload

---
+++ 1.1.5/! Robot maze2/main.py
@@ -0,0 +1,119 @@
+# a115_robot_maze.py
+import turtle
+
+# ----- maze and turtle config variables
+screen_h = 400
+screen_w = 420
+startx = -100
+starty = -100
+turtle_scale = 1.5
+
+# ------ robot commands
+
+
+def move(x=1):
+    for i in range(x):
+        robot.dot(10)
+        robot.fd(50)
+
+
+def turn_left():
+    robot.speed(0)
+    robot.lt(90)
+    robot.speed(2)
+
+
+def turn_right():
+    robot.speed(0)
+    robot.lt(-90)
+    robot.speed(2)
+
+# ----- init screen
+wn = turtle.Screen()
+wn.setup(width=screen_w, height=screen_h)
+robot_image = "assets/robot.gif"
+wn.addshape(robot_image)
+
+# ----- init robot
+robot = turtle.Turtle(shape=robot_image)
+robot.hideturtle()
+robot.color("darkorchid")
+robot.pencolor("darkorchid")
+robot.penup()
+robot.setheading(90)
+robot.turtlesize(turtle_scale, turtle_scale)
+robot.goto(startx, starty)
+robot.speed(2)
+robot.showturtle()
+
+# ---- TODO: change maze here
+wn.bgpic("assets/maze3.png")  # other file names should be maze2.png, maze3.png
+
+# ---- TODO: begin robot movement here
+def solveOne():
+    w1 = 0
+    while w1 < 2:
+        move()
+        w1 += 1
+    turn_right()
+    w1 = 0
+    while w1 < 2:
+        move()
+        w1 += 1
+    w1 = 0
+    while w1 < 2:
+        move()
+        turn_left()
+        move()
+        turn_right()
+        w1 += 1
+def solveTwo():
+    w2 = 0
+    while w2 < 3:
+        move()
+        w2 += 1
+    turn_right()
+    w2 = 0
+    while w2 < 2:
+        move()
+        w2 += 1
+    w2 = 0
+    robot.goto(startx,starty)
+    while w2 < 3:
+        move() #The move function IS a nested loop. I changed it to have a for loop and a variable to decide how many times to run.
+        w2 += 1
+    w2 = 0
+    turn_left()
+    while w2 < 3:
+        move()
+        w2 += 1
+    w2 = 0
+    while w2 < 1:
+        turn_left()
+        move()
+        w2+=1
+def solveThree():
+    w3 = 0
+    turn_right()
+    move()
+    while w3 < 4:
+        turn_left()
+        move()
+        turn_right()
+        w3 += 1
+        if w3 < 4:
+            move()
+
+if wn.bgpic() == "assets/maze1.png":
+    solveOne()
+elif wn.bgpic() == "assets/maze2.png":
+    solveTwo()
+elif wn.bgpic() == "assets/maze3.png":
+    solveThree()
+else:
+    wn.exitonclick()
+
+
+# ---- end robot movement
+
+wn.mainloop()
---
+++ 1.1.5/README.md
@@ -0,0 +1,9 @@
+# 1.1.5 Robot Maze
+
+## Files
+
+- [! Robot maze1](!%20Robot%20maze1) (A folder containing [all neccecary files](!%20Robot%20maze1/assets) to run [main.py](!%20Robot%20maze1/main.py) with maze 1)
+- [! Robot maze2](!%20Robot%20maze2) (A folder containing [all neccecary files](!%20Robot%20maze2/assets) to run [main.py](!%20Robot%20maze2/main.py) with maze 1)
+- [! Robot maze3](!%20Robot%20maze3) (A folder containing [all neccecary files](!%20Robot%20maze3/assets) to run [main.py](!%20Robot%20maze3/main.py) with maze 1)
+
+Refer to README.MD of AP in main branch if files dirs are not found.
