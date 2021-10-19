49b3195f0ed6d9bc0e275eb6f0f14e0fab869d1d
Andrew Guernsey <andrewgguernsey@gmail.com>
2021-10-19 09:51:12 -0500

    Did a lot of stuff. Like a whole lot.

--- ELC2/main.py
+++ ELC2/main.py
@@ -1,5 +1,12 @@
-import os
-
+try:
+    from Classes.colors import txtcolor
+except ImportError:
+    raise Exception("#000 Connected Classes not found!")
+try:
+    import os
+    import pyCardDeck as pcd
+except ImportError:
+    raise Exception(f"{txtcolor.FAIL}A dependancy could not be accessed! Refer to error code #001 for solution!{txtcolor.ENDC}")
 def cls(): os.system('cls' if os.name=='nt' else 'clear')
 def cardIn(setVar):
     setVar = input("Input cards >> ")
@@ -9,5 +16,4 @@ def cardIn(setVar):
 cls()
 allcards = []
 while True:
-    
     cardIn(allcards)
