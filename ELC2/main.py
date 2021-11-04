58452cb6b723e20592b2e84ac239b58fd09fee97
jayhawker6 <andrewgguernsey@gmail.com>
2021-11-04 08:47:10 -0500

    added enemy section

--- ELC2/main.py
+++ ELC2/main.py
@@ -10,13 +10,58 @@ except ImportError:
 def cls(): os.system('cls' if os.name=='nt' else 'clear')
 def cardIn(setVar):
     setVar = input("Input cards >> ")
+def printWarn(warningText):
+    if warningText == None: raise Exception("A warning was not defined in the code")
+    print(f"{txtcolor.WARNING}%s{txtcolor.ENDC}" % (warningText)), os.system('Pause'), cls()
+    
+    
+cls()
     
 ## BEGIN INTERFACE ##
 
-cls()
-input = input("Card values seperated by comma and space >> ")
-enemyNumber = "K"
-validNumbers = [1,2,3,4,5,6,7,8,9,"T","J","Q","K","A"]
-while enemyNumber not in range(1, 10, 1):
-    if enemyNumber not in validNumbers:
-        raise Exception(f"{txtcolor.WARNING} CARD RANK NOT IDENTIFIED CORRECTLY!{txtcolor.ENDC}")
+print("""
+INFO:
+    The given input must be in the correct format,
+    Looking like this: (Excluding the parethacies)
+                      #X, #X, #X, #X, #X, #X
+    It should have exactly 22 characters, the first
+    value given is the card of the opponent, and the
+    last 5 cards are the hand of the dealer. # represents
+    the rank and X represents the suit.
+    Valid ranks are in the following order:
+    A (Ace)
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    T (10)
+    J (Jack)
+    Q (Queen)
+    K (King)
+    
+    Valid suits:
+    D (Diamonds)
+    C (Clubs)
+    H (Hearts)
+    S (Spades)
+    
+""")
+os.system('pause'), cls()
+validRanks = [1,2,3,4,5,6,7,8,9,"T","J","Q","K","A"]
+validSuits = ["D","H","C","S"]
+while True:
+    userinput = str(input("Card values seperated by one comma and one space >> "))
+    if len(userinput) != 22:
+        printWarn(" -=-  INPUT LENGTH IS INCORRECT!  -=- ")
+        continue
+    elif userinput[0] not in validRanks or userinput[1] not in validSuits:
+        printWarn(" -=-  CARD VALUE NOT IDENTIFIED!  -=- ")
+        continue
+    else:
+        enemyCard = userinput[0]
+    
--- ELC2/txt/EL_Challenge_02.md
+++ ELC2/txt/EL_Challenge_02.md
@@ -1,41 +1,19 @@
 ﻿# EXTENDED LEARNING CHALLENGE 2
 
-PROBLEM: AGRAM is a card game for 2 players, using the cards from a standard 52-card
-
-pack.
-
-The dealer deals five cards to each player. The opponent player leads any card, playing it face up
-
-in the middle of the playing area.
-
-For this program the following strategy will be used to determine which card the dealer will play:
-
+PROBLEM: AGRAM is a card game for 2 players, using the cards from a standard 52-card pack. The dealer deals five cards to each player. The opponent player leads any card, playing it face up in the middle of the playing area. For this program the following strategy will be used to determine which card the dealer will play:
 The dealer must play a card of the same suit if he can.
-
-He plays the lowest card in that suit that is of a higher rank than the card the opponent
-
-played.
-
+He plays the lowest card in that suit that is of a higher rank than the card the opponent played.
 If he does not have such a card, he plays his lowest card in that suit.
-
 If he does not have a card in that suit, he plays the lowest ranking card regardless of suit.
-
 We guarantee there will be no ties.
-
 INPUT: There will be 5 lines of input. Each line will contain the opponent’s lead card and the
-
 5 cards held by the dealer. All cards will be represented by 2-character strings in value-suit
-
 order. AH represents the ace of hearts. K, Q, J, and T will be used for king, queen, jack and 10
-
 respectively. Note that the ace in this game has the lowest rank.
-
 OUTPUT: For each input line print the card the dealer must play according to the strategy listed
-
 above.
 
 SAMPLE INPUT
-
 SAMPLE OUTPUT
 
 \1. 9D
