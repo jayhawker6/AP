0d6f51bfeb26c4eb8798e6e1a9eeecde29e49ecc
jayhawker6 <andrewgguernsey@gmail.com>
2021-11-04 09:46:27 -0500

    Leaving CS

--- ELC2/main.py
+++ ELC2/main.py
@@ -13,55 +13,28 @@ def cardIn(setVar):
 def printWarn(warningText):
     if warningText == None: raise Exception("A warning was not defined in the code")
     print(f"{txtcolor.WARNING}%s{txtcolor.ENDC}" % (warningText)), os.system('Pause'), cls()
+def translateRank(transvar,tolist):
+    if transvar == "T": tolist = [10,tolist]
+    if transvar == "J": tolist = [11,tolist]
+    if transvar == "Q": tolist = [12,tolist]
+    if transvar == "Q": tolist = [12,tolist]
+
     
     
 cls()
     
 ## BEGIN INTERFACE ##
 
-print("""
-INFO:
-    The given input must be in the correct format,
-    Looking like this: (Excluding the parethacies)
-                      #X, #X, #X, #X, #X, #X
-    It should have exactly 22 characters, the first
-    value given is the card of the opponent, and the
-    last 5 cards are the hand of the dealer. # represents
-    the rank and X represents the suit.
-    Valid ranks are in the following order:
-    A (Ace)
-    1
-    2
-    3
-    4
-    5
-    6
-    7
-    8
-    9
-    T (10)
-    J (Jack)
-    Q (Queen)
-    K (King)
-    
-    Valid suits:
-    D (Diamonds)
-    C (Clubs)
-    H (Hearts)
-    S (Spades)
-    
-""")
+print("""\nINFO:\n    The given input must be in the correct format,\n    Looking like this: (Excluding the parethacies)\n    #X, #X, #X, #X, #X, #X\n    It should have exactly 22 characters, the first\n    value given is the card of the opponent, and the\n    last 5 cards are the hand of the dealer. # represents\n    the rank and X represents the suit.\n    Valid ranks are in the following order:\n    A (Ace)\n    1\n    2\n    3\n    4\n    5\n    6\n    7\n    8\n    9\n    T (10)\n    J (Jack)\n    Q (Queen)\n    K (King)\n    \n    Valid suits:\n    D (Diamonds)\n    C (Clubs)\n    H (Hearts)\n    S (Spades)\n    \n""")
 os.system('pause'), cls()
 validRanks = [1,2,3,4,5,6,7,8,9,"T","J","Q","K","A"]
 validSuits = ["D","H","C","S"]
 while True:
     userinput = str(input("Card values seperated by one comma and one space >> "))
-    if len(userinput) != 22:
-        printWarn(" -=-  INPUT LENGTH IS INCORRECT!  -=- ")
-        continue
-    elif userinput[0] not in validRanks or userinput[1] not in validSuits:
-        printWarn(" -=-  CARD VALUE NOT IDENTIFIED!  -=- ")
-        continue
-    else:
-        enemyCard = userinput[0]
-    
+    if len(userinput) != 22: printWarn(" -=-  INPUT LENGTH IS INCORRECT!  -=- "); continue
+    if userinput[0] == "T": enemyCard = [10,userinput[1]]
+    if userinput[0] == "J": enemyCard = [11,userinput[1]]
+    if userinput[0] == "Q": enemyCard = [12,userinput[1]]
+    if int(userinput[1]) not in validRanks: printWarn(" -=-  CARD VALUE NOT IDENTIFIED!  -=- "); continue
+    else: break
+print(f"{txtcolor.OKGREEN}Program ENDED!{txtcolor.ENDC}")
