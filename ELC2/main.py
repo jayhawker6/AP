67680014a7147e51b1f5b281e9fced9358a48f5b
jayhawker6 <andrewgguernsey@gmail.com>
2021-11-26 13:51:26 -0600

    IT IS DONE

--- ELC2/main.py
+++ ELC2/main.py
@@ -11,18 +11,22 @@ def printWarn(warningText):
     if warningText == None: raise Exception("A warning was not defined in the code")
     print(f"{txtcolor.WARNING}%s{txtcolor.ENDC}" % (warningText)), os.system('Pause'), cls()
 def translateRank(transvar):
-    if transvar == "A": return 0
+    if transvar == "A": return 1
     if transvar == "T": return 10
     if transvar == "J": return 11
     if transvar == "Q": return 12
     if transvar == "K": return 13
+    else: return int(transvar)
 cls()
+def progend():
+    print(f"{txtcolor.OKGREEN}Program ENDED!{txtcolor.ENDC}")
+    os.system("Exit")
     
 ## BEGIN INTERFACE ##
 
-print("""\nINFO:\n    The given input must be in the correct format,\n    Looking like this: (Excluding the parethacies)\n    #X, #X, #X, #X, #X, #X\n    It should have exactly 22 characters, the first\n    value given is the card of the opponent, and the\n    last 5 cards are the hand of the dealer. # represents\n    the rank and X represents the suit.\n    Valid ranks are in the following order:\n    A (Ace)\n    1\n    2\n    3\n    4\n    5\n    6\n    7\n    8\n    9\n    T (10)\n    J (Jack)\n    Q (Queen)\n    K (King)\n    \n    Valid suits:\n    D (Diamonds)\n    C (Clubs)\n    H (Hearts)\n    S (Spades)\n    \n""")
+print(f"""{txtcolor.OKBLUE}\nINFO:\n    The given input must be in the correct format,\n    Looking like this: (Excluding the parethacies)\n    #X, #X, #X, #X, #X, #X\n    It should have exactly 22 characters, the first\n    value given is the card of the opponent, and the\n    last 5 cards are the hand of the dealer. # represents\n    the rank and X represents the suit.\n    Valid ranks are in the following order:\n    A (Ace)\n    2\n    3\n    4\n    5\n    6\n    7\n    8\n    9\n    T (10)\n    J (Jack)\n    Q (Queen)\n    K (King)\n    \n    Valid suits:\n    D (Diamonds)\n    C (Clubs)\n    H (Hearts)\n    S (Spades)\n    \n{txtcolor.ENDC}""")
 os.system('pause'), cls()
-validRanks = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+validRanks = [1,2,3,4,5,6,7,8,9,10,11,12,13]
 validSuits = ["D","H","C","S"]
 while True:
     
@@ -34,10 +38,8 @@ while True:
     
     if enemyrank not in validRanks or enemysuit not in validSuits: printWarn(" -=-  ENEMY CARD VALUE NOT IDENTIFIED!  -=- "); continue #Check that the translation output a correct numerical value
     
-    print(f"{txtcolor.FAIL}Rank of enemy = %s{txtcolor.ENDC}" % (enemyrank)), os.system('pause')
-    dealerhand = userinput[4:22].split(", ")
-    dc1 = dealerhand[0]
-    print(dealerhand)
+    print(f"{txtcolor.FAIL}Rank of enemy = %s{txtcolor.ENDC}" % (enemyrank)), os.system('pause'), cls()
+    dealerhand = tuple(userinput[4:22].split(", "))
     break
 
 coss = [] #List containing cards in dealer's hand in the same suit of that of the opponents (Cards Of Same Suit)
@@ -46,8 +48,15 @@ PrintValues = {
     "H":"Hearts",
     "C":"Clubs",
     "S":"Spades",
-    1:"Ace",
     "A":"Ace",
+    2:"2",
+    3:"3",
+    4:"4",
+    5:"5",
+    6:"6",
+    7:"7",
+    8:"8",
+    9:"9",
     "T":"Ten",
     "J":"Jack",
     "Q":"Queen",
@@ -114,9 +123,25 @@ for i in range(len(dealerhand)):
 if not coss:
     lowestcard = CardOrder[dealerhand[0]]
     for i in range(len(dealerhand)):
-        if dealerhand[i] in CardOrder < lowestcard in CardOrder:
-            lowestcard = i
-    print(dealerhand[lowestcard])
-        
-
-print(f"{txtcolor.OKGREEN}Program ENDED!{txtcolor.ENDC}")
+        if CardOrder[dealerhand[i]] < CardOrder[dealerhand[lowestcard]]:
+            lowestcard = int(i-1)
+    printWarn("The dealer plays the %s of %s" % (PrintValues[coss[lowestcard[0]]],PrintValues[coss[lowestcard[1]]]))
+    progend()
+else:
+    bestcardinsuit = coss[0]
+    worstcardinsuit = coss[0]
+    for i in range(len(coss)):
+        if CardOrder[coss[i]] > CardOrder[bestcardinsuit]:
+            bestcardinsuit = coss[i]
+    if CardOrder[bestcardinsuit] <= enemyrank:
+        for i in range(len(coss)):
+            if CardOrder[coss[i]] < CardOrder[worstcardinsuit]:
+                worstcardinsuit = coss[i]
+        printWarn("The dealer plays the %s of %s" % (PrintValues[worstcardinsuit[0]],PrintValues[worstcardinsuit[1]]))
+        progend()
+    else:
+        for i in range(len(coss)):
+            if CardOrder[coss[int(i)]] < CardOrder[bestcardinsuit] and CardOrder[coss[int(i)]] > enemyrank:
+                bestcardinsuit = coss[int(i)]
+        printWarn("The dealer plays the %s of %s" % (PrintValues[int(bestcardinsuit[0])],PrintValues[bestcardinsuit[1]]))
+        progend()
